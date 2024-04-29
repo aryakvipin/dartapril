62dd4ab4481565a508565ec874bb9894b4957aa6
aryakvipin <arya28985@gmail.com>
2024-04-29 11:19:57 +0530

    commit

---
+++ bin/elseifladder.dart
@@ -0,0 +1,22 @@
+void main(){
+   String day ="friday";
+   if(day=="sunday"){
+     print("sunday");
+   }
+   else if(day=="monday"){
+     print("monday");
+   }
+   else if(day=="tuesday"){
+     print("tuesday");
+   }else if(day=="wednesday"){
+     print("wednesday");
+   }
+   else if(day=="thursday"){
+     print("thursday");
+   }else if(day=="friday"){
+     print("friday");
+   }
+   else{
+     print("Saturday");
+   }
+}
